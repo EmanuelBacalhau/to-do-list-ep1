37987236247d21c3ff856c108f95f997ffec52ab
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-10-30 17:48:27 -0300

    feat: add controller, service and router for delete list

--- src/routes/list-routes.ts
+++ src/routes/list-routes.ts
@@ -3,9 +3,11 @@ import { Router } from 'express'
 import createListController from '../modules/controllers/list/create-list.controller'
 import detailsListController from '../modules/controllers/list/details-list.controller'
 import updateListController from '../modules/controllers/list/update-list.controller'
+import deleteListController from '../modules/controllers/list/delete-list.controller'
 
 export const listRouter = Router()
 
 listRouter.post('/lists/register', createListController.handle)
 listRouter.get('/lists/:id', detailsListController.handle)
 listRouter.put('/lists/:id', updateListController.handle)
+listRouter.put('/lists/:id', deleteListController.handle)
