550f5d72ea4a6e9057609282ada26bfc16b363ff
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-10-31 17:24:58 -0300

    feat: add controller, service and router for update task

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import createTaskController from '../modules/controllers/task/create-task.controller'
 import detailsTaskController from '../modules/controllers/task/details-task.controller'
+import updateTaskController from '../modules/controllers/task/update-task.controller'
 
 export const taskRouter = Router()
 
 taskRouter.post('/tasks/register', createTaskController.handle)
 taskRouter.get('/tasks/:id', detailsTaskController.handle)
+taskRouter.put('/tasks/:id', updateTaskController.handle)
