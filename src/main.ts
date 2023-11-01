ef80ca8f89ec5118991f982b3df59195db6ffd63
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-11-01 15:44:09 -0300

    feat: add controller, service and router for auth user

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ import { errorHandle } from './middlewares/error-handling'
 import { userRouter } from './routes/user-routes'
 import { listRouter } from './routes/list-routes'
 import { taskRouter } from './routes/task-routes'
+import { authRouter } from './routes/auth-routes'
 
 const server = express()
 
 server.use(express.json())
 
-server.use('/api', [userRouter, listRouter, taskRouter])
+server.use('/api', [userRouter, listRouter, taskRouter, authRouter])
 
 server.use(errorHandle)
 
