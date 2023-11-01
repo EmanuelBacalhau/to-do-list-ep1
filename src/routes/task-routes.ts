203f05119fbfa868ac634e9b411eef22d9c630cd
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-10-31 21:44:26 -0300

    feat: add controller, service and route for delete task

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 import createTaskController from '../modules/controllers/task/create-task.controller'
 import detailsTaskController from '../modules/controllers/task/details-task.controller'
 import updateTaskController from '../modules/controllers/task/update-task.controller'
+import deleteTaskController from '../modules/controllers/task/delete-task.controller'
 
 export const taskRouter = Router()
 
 taskRouter.post('/tasks/register', createTaskController.handle)
 taskRouter.get('/tasks/:id', detailsTaskController.handle)
 taskRouter.put('/tasks/:id', updateTaskController.handle)
+taskRouter.delete('/tasks/:id', deleteTaskController.handle)
