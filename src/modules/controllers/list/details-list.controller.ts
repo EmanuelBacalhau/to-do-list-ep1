190d793a8dba6745b8c3a551ba8566f90435aa21
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-11-07 07:58:01 -0300

    fix: correcting bug in search list details

--- src/modules/controllers/list/details-list.controller.ts
+++ src/modules/controllers/list/details-list.controller.ts
@@ -3,10 +3,14 @@ import { z } from 'zod'
 import detailsListService from '../../services/list/details-list.service'
 
 class DetailsListController {
-  async handle(req: Request, res: Response) { 
-    const id = req.id
+  async handle(req: Request, res: Response) {
+    const detailsListSchema = z.object({
+      id: z.string().uuid(),
+    })
 
-    const list = await detailsListService.execute({ id })
+    const data = detailsListSchema.parse(req.params)
+
+    const list = await detailsListService.execute(data)
 
     return res.status(200).json(list)
   }
