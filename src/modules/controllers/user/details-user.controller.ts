468d42e93ffc1e78cba4928f5317dd649571eef0
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-11-07 07:57:26 -0300

    feat: optimizing controller for search user details

--- src/modules/controllers/user/details-user.controller.ts
+++ src/modules/controllers/user/details-user.controller.ts
@@ -1,16 +1,11 @@
 import { Request, Response } from 'express'
-import { z } from 'zod'
 import detailsUserService from '../../services/user/details-user.service'
 
 class DetailsUserController {
   async handle(req: Request, res: Response) {
-    const detailsUserSchema = z.object({
-      id: z.string().uuid(),
-    })
+    const id = req.id
 
-    const data = detailsUserSchema.parse(req.params)
-
-    const user = await detailsUserService.execute(data)
+    const user = await detailsUserService.execute({ id })
 
     return res.status(200).json(user)
   }
